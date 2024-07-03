921d47a532a1ae8a61e3b40249948fec5a943420
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-07-03 20:38:47 -0300

    feat: Add OrderEntity and OrderItemEntity classes

---
+++ src/main/java/br/com/rafael/orderms/entity/OrderEntity.java
@@ -0,0 +1,56 @@
+package br.com.rafael.orderms.entity;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Document(collection = "tb_orders")
+public class OrderEntity {
+    @MongoId
+    private Long orderId;
+    @Indexed(name = "customer_id_index")
+    private String customerId;
+    @Field(targetType = FieldType.DECIMAL128)
+    private BigDecimal total;
+    private List<OrderItemEntity> items;
+
+    public OrderEntity() {
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<OrderItemEntity> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItemEntity> items) {
+        this.items = items;
+    }
+}
