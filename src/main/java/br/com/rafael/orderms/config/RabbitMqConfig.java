65e0c953a8b7b782a98a33c4d298814b01dd0050
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-07-03 20:30:06 -0300

    feat: Add RabbitMqConfig for order creation queue

---
+++ src/main/java/br/com/rafael/orderms/config/RabbitMqConfig.java
@@ -0,0 +1,8 @@
+package br.com.rafael.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+    public static final String ORDER_CREATED_QUEUE = "order-created";
+}
